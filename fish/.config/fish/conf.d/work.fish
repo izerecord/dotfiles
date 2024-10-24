425558a6857c0b8427348268b418e4a7aeaafdc0
Christian Handschuh <christian.handschuh@komm.one>
2024-10-24 11:31:36 +0200

    add proxy and pipx path for work

--- fish/.config/fish/conf.d/work.fish
+++ fish/.config/fish/conf.d/work.fish
@@ -1,9 +1,10 @@
 #!/usr/bin/env fish
 # not needed for now, because the workaround of 
 # https://github.com/microsoft/vscode-remote-release/issues/2509#issuecomment-1640162892
-exit
 if test (hostname) = casdev
-    # clear
+    # pipx
+    set PATH $PATH $HOME/.local/bin
+    # proxy
     if test -z "$http_proxy"
         exec bash -c "source /data/scripts/set_proxy.sh >/dev/null 2>&1; exec fish"
         clear
