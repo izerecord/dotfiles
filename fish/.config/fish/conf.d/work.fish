6840124ef6030b70ca8f123301d9835939f36785
Christian Handschuh <c.handschuh@posteo.de>
2024-10-27 23:17:45 +0100

    add home.fish

--- fish/.config/fish/conf.d/work.fish
+++ fish/.config/fish/conf.d/work.fish
@@ -1,6 +1,4 @@
 #!/usr/bin/env fish
-# not needed for now, because the workaround of 
-# https://github.com/microsoft/vscode-remote-release/issues/2509#issuecomment-1640162892
 if test (hostname) = casdev
     # pipx
     set PATH $PATH $HOME/.local/bin
