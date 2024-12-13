5b933938a732c932f0e8175889a7bf3608ed6dbc
Christian Handschuh <christian.handschuh@komm.one>
2024-12-13 19:50:28 +0100

    add flux and task completions and go path

--- fish/.config/fish/conf.d/work.fish
+++ fish/.config/fish/conf.d/work.fish
@@ -2,6 +2,9 @@
 if test (hostname) = casdev
     # pipx
     set PATH $PATH $HOME/.local/bin
+    # go
+    set PATH $PATH $HOME/go/bin
+    set -Ux PATH $GOPATH/bin $PATH
     # proxy
     if test -z "$http_proxy"
         exec bash -c "source /data/scripts/set_proxy.sh >/dev/null 2>&1; exec fish"
