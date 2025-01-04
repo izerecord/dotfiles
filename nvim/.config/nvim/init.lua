94da06761a2afe586363f32a1d8726548faa5e9d
Christian <chris@macbook-air.local>
2025-01-04 16:02:28 +0100

    feat: add brew path

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,4 +1,5 @@
-vim.opt.expandtab = true      -- Use spaces instead of tabs
-vim.opt.tabstop = 4           -- Number of spaces that a <Tab> counts for
-vim.opt.shiftwidth = 4        -- Number of spaces to use for each step of (auto)indent
-vim.opt.softtabstop = 4       -- Number of spaces a <Tab> counts while editing
+---@diagnostic disable: undefined-global
+vim.opt.expandtab = true -- Use spaces instead of tabs
+vim.opt.tabstop = 4      -- Number of spaces that a <Tab> counts for
+vim.opt.shiftwidth = 4   -- Number of spaces to use for each step of (auto)indent
+vim.opt.softtabstop = 4  -- Number of spaces a <Tab> counts while editing
