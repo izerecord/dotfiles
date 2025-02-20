a7ae59aabf63c57fde57f32ff5164db7c81d05f9
Christian Handschuh <christian.handschuh@komm.one>
2025-02-20 19:00:31 +0100

    feat: show tree with fzf directory search

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -4,6 +4,8 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+# TODO: status is-interactive || exit
+
 starship init fish | source
 fzf --fish | source
 
@@ -11,3 +13,6 @@ fzf --fish | source
 # prerequisits: plugin `patrickf1/fzf.fish`
 fzf_configure_bindings --directory=\cf --git_log=\cg --git_status=\cs --history=\cr --variables=\ce --processes=\cp
 set fzf_fd_opts --hidden --max-depth 5 --ignore
+set fzf_preview_dir_cmd lsd --tree --all --color=always --icon=always
+# set fzf_preview_file_cmd
+# set fzf_git_log_format 
