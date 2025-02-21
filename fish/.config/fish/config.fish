25e1b9027e97927779c0881cadd80e56d7a85dbc
Christian Handschuh <c.handschuh@posteo.de>
2025-02-21 20:33:56 +0100

    feat: add catpucchin theme for btop, add ghostty, reconfigure fzf theme and, configure tmux

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,18 +1,31 @@
 source ~/.config/fish/alias.fish
 
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
-
-# TODO: status is-interactive || exit
+status is-interactive || exit
 
 starship init fish | source
 fzf --fish | source
 
+###############################################################################
 # fzf specifics
 # prerequisits: plugin `patrickf1/fzf.fish`
-fzf_configure_bindings --directory=\cf --git_log=\cg --git_status=\cs --history=\cr --variables=\ce --processes=\cp
+###############################################################################
+fzf_configure_bindings \
+    --directory=\cf \
+    --git_log=\cg \
+    --git_status=\cs \
+    --history=\cr \
+    --variables=\ce \
+    --processes=\cp
+# search directory
 set fzf_fd_opts --hidden --max-depth 5 --ignore
+# preview directories with `lsd --tree`
 set fzf_preview_dir_cmd lsd --tree --all --color=always --icon=always
+
+# fzf catpucchin and 50% height
+set -Ux FZF_DEFAULT_OPTS "\
+   --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
+   --color=marker:#b4befe,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 \
+   --color=selected-bg:#45475a \
+   --height 40% --tmux bottom,80%,40% --layout reverse --border top"
 # set fzf_preview_file_cmd
 # set fzf_git_log_format 
