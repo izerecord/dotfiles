a48f7f4ef3bb7cd048b62881b3610bb5a5ab1cc5
Christian Handschuh <c.handschuh@posteo.de>
2025-02-22 00:11:46 +0100

    fix: fish vars

--- fish/config.fish
+++ fish/config.fish
@@ -20,16 +20,18 @@ fzf_configure_bindings \
     --history=\cr \
     --variables=\ce \
     --processes=\cp
+
 # search directory
-set fzf_fd_opts --hidden --max-depth 5 --ignore
+set -x fzf_fd_opts --hidden --max-depth 5 --ignore
 # preview directories with `lsd --tree`
-set fzf_preview_dir_cmd lsd --tree --all --color=always --icon=always
+set -x fzf_preview_dir_cmd lsd --tree --all --color=always --icon=always
 
 # fzf catpucchin and 50% height
-set -Ux FZF_DEFAULT_OPTS "\
+set -x FZF_DEFAULT_OPTS "\
    --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
    --color=marker:#b4befe,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 \
    --color=selected-bg:#45475a \
    --height 40% --tmux bottom,80%,40% --layout reverse --border top"
 # set fzf_preview_file_cmd
 # set fzf_git_log_format 
+
