4b7d79f0c940c14e4343717c21a015cca53cc97b
Christian Handschuh <c.handschuh@posteo.de>
2025-02-22 21:42:44 +0100

    add --no-folding to stowrc and clean some files

--- fish/alias.fish
+++ fish/alias.fish
@@ -2,6 +2,7 @@
 # alias git=hub
 alias vim=nvim
 alias ls=lsd
+alias tree="lsd --tree"
 
 # cat bat catbat madness
 if test -f /etc/os-release
--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,5 @@
----@diagnostic disable: undefined-global
 vim.opt.expandtab = true -- Use spaces instead of tabs
 vim.opt.tabstop = 4      -- Number of spaces that a <Tab> counts for
-vim.opt.shiftwidth = 4   -- Number of spaces to use for each step of (auto)indent
 vim.opt.softtabstop = 4  -- Number of spaces a <Tab> counts while editing
+vim.opt.shiftwidth = 4   -- Number of spaces to use for each step of (auto)indent
+
