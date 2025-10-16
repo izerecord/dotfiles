8f516a45162f711450f40b4b23bdddbe808c9446
Christian Handschuh <c.handschuh@posteo.de>
2025-10-16 21:26:09 +0200

    update starship and stabilize fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -23,21 +23,25 @@ end
 # prerequisits: plugin `patrickf1/fzf.fish`
 # https://github.com/PatrickF1/fzf.fish
 ###############################################################################
-fzf --fish | source
-fzf_configure_bindings \
-    --directory=\cf \
-    --git_log=\cb \
-    --git_status=\cg \
-    --history=\cr \
-    --variables=\ce \
-    --processes=\cp
-# search directories
-set -x fzf_fd_opts --hidden --ignore
-set -x fzf_preview_dir_cmd eza --tree --all --color=always --icons=auto
-set fzf_preview_file_cmd bat --color=always --style=numbers
-#set fzf_git_log_format "%s"
-set fzf_diff_highlighter DFT_DISPLAY=inline DFT_COLOR=always git show --color=always --stat --patch --ext-diff {1}
+if type -q fzf
+    fzf --fish | source
+end
 
+if type -q fzf_configure_bindings
+    fzf_configure_bindings \
+        --directory=\cf \
+        --git_log=\cb \
+        --git_status=\cg \
+        --history=\cr \
+        --variables=\ce \
+        --processes=\cp
+    # search directories
+    set -x fzf_fd_opts --hidden --ignore
+    set -x fzf_preview_dir_cmd eza --tree --all --color=always --icons=auto
+    set fzf_preview_file_cmd bat --color=always --style=numbers
+    #set fzf_git_log_format "%s"
+    set fzf_diff_highlighter DFT_DISPLAY=inline DFT_COLOR=always git show --color=always --stat --patch --ext-diff {1}
+end
 #set -x FZF_DEFAULT_OPTS "\
 #   --height 40% --tmux bottom,100%,40% --layout reverse --border top"
 
