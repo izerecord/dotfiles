2d3e33564469aba4abc8be9d13b2f0f970d8693d
Christian Handschuh <c.handschuh@posteo.de>
2026-02-22 10:33:38 +0100

    feat: add sunshine configs

---
+++ sunshine/.config/sunshine/prep/do.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+hyprctl keyword monitorv2[DP-1]:disabled 1
+hyprctl keyword monitorv2[HDMI-A-1]:disabled 1
+hyprctl dispatch focusmonitor DP-3
---
+++ sunshine/.config/sunshine/prep/undo.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+hyprctl keyword monitorv2[DP-1]:disabled 0
+hyprctl keyword monitorv2[HDMI-A-1]:disabled 0
+hyprctl dispatch focusmonitor DP-1
